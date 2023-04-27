d90c26debf019920ba926e0e21a8878e33e27edb
John Grohman <grohmanjp@gmail.com>
2023-04-26 19:45:16 -0500

    Changed Dashboard to have it's own css file

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import "../../App.css";
+import Header from "../../components/Header/index.js";
+import "./index.css";
+import Shelf from "../../components/shelf/index.js";
 
 const Dashboard = () => (
     <div id="dashboard">
+        <Header />
         <h1>Dashboard</h1>
-        
+        <Shelf />
     </div>
 )
 
