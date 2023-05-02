3369bf99680d8578bbdb977b222eb08f1d6c2ae4
John Grohman <grohmanjp@gmail.com>
2023-05-02 10:44:38 -0500

    fixed onboarding issue

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -11,11 +11,10 @@ class  Login extends React.Component {
         this.state = {isLogin: true};
 
         // Necessary to make 'this' work in the callback
-        // this.handleButtons = this.handleButtons.bind(this);
+        this.handleButtons = this.handleButtons.bind(this);
     }
 
     handleButtons() {
-        console.log("Hello World");
         this.setState(prevState => ({isLogin:!prevState.isLogin}));
     }
 
@@ -25,40 +24,44 @@ class  Login extends React.Component {
                 <h1>Closet Catalog</h1>
                 <p>Welcome to the ClosetCatalog! This is a database that you can access anywhere on the internet.</p>
                 
-                {(this.state.isLogin) ? 
-                (<div id="card" class="center">
+                {(this.state.isLogin) ?
+                (<div id="card" className="center">
                     <h2>Login</h2>
                     <p>Please login or create an account.</p>
                     <form>
-                        <label for="uname"><b>Username: </b></label>
-                        <input type="text" placeholder="Enter Username" name="uname" required></input>
+                        <b>Username: </b>
+                        <input type="text" placeholder="Enter Username" id="uname" name="uname" required></input>
                         <br></br><br></br>
-                        <label for="psw"><b>Password: </b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" required></input>
+                        <b>Password: </b>
+                        <input type="password" placeholder="Enter Password" id="psw" name="psw" required></input>
                         <br></br><br></br>
-                        <button type="submit" class="grayedout">Login</button>
-                        <button type="button">Create Account</button>
+                        <button type="submit">Login</button>
+                        <br></br><br></br>
+                        <b>Don't have an Account? </b>
+                        <button type="button" onClick={() => this.handleButtons()}>Create</button>
                     </form>
                 </div>)
-            :
-            (<div id="card" class="center">
-                <h2>Create an Account</h2>
-                <p>Create an account with Closet Catalog</p>
-                <form>
-                    <label for="uname"><b>Username: </b></label>
-                    <input type="text" placeholder="Enter Username" name="uname" required></input>
-                    <br></br><br></br>
-                    <label for="psw"><b>Password: </b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required></input>
-                    <br></br><br></br>
-                    <button type="submit">Login</button>
-                    <button type="button" class="grayedout">Create Account</button>
-                </form>
-            </div>)}
+                :
+                (<div id="card" className="center">
+                    <h2>Create an Account</h2>
+                    <p>Create an account with Closet Catalog</p>
+                    <form>
+                        <b>Username: </b>
+                        <input type="text" placeholder="Enter Username" name="uname" required></input>
+                        <br></br><br></br>
+                        <b>Password: </b>
+                        <input type="password" placeholder="Enter Password" name="psw" required></input>
+                        <br></br><br></br>
+                        <button type="submit">Create Account</button>
+                        <br></br><br></br>
+                        <b>Already have an Account? </b>
+                        <button type="button" onClick={() => this.handleButtons()}>Login</button>
+                    </form>
+                </div>
+                )}
             </div>
         )
     }
-
 }
 
 export default Login;
