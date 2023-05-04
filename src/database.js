c078b45b83e85e28077a4d1ef75a4b8cfdeb95ab
MLicari1 <114188861+MLicari1@users.noreply.github.com>
2023-05-04 01:11:31 -0500

    fixed database.js

--- src/database.js
+++ src/database.js
@@ -10,12 +10,11 @@ db.exec(`
     );
 
     CREATE TABLE IF NOT EXISTS closet (
-    CREATE TABLE IF NOT EXISTS closet (
         Cid           INTEGER PRIMARY KEY AUTOINCREMENT,
         name          TEXT NOT NULL
     );
 
-    CREATE TABLE IF NOT EXISTS IF NOT EXISTS helf (
+    CREATE TABLE IF NOT EXISTS shelf (
         Sid           INTEGER PRIMARY KEY AUTOINCREMENT,
         size          INTEGER NOT NULL,
         material      TEXT NOT NULL,
@@ -36,28 +35,28 @@ db.exec(`
     );
 
     CREATE TABLE IF NOT EXISTS Owns (
-        user_id         INTEGER FOREIGNKEY,//is this how you denote a foreign key in SQL?
-        closet_id       INTEGER FOREIGNKEY,
-        PRIMARY KEY (user_id, closet_id)//I don't think this is the right syntax... can't use PRIMARY KEY constraint as normal tho since there are two
-        //to be finished
+        user_id         INTEGER,--is this how you denote a foreign key in SQL?
+        closet_id       INTEGER,
+        PRIMARY KEY (user_id, closet_id)--I don't think this is the right syntax... can't use PRIMARY KEY constraint as normal tho since there are two
+        --to be finished
     );
 
     CREATE TABLE IF NOT EXISTS Belongs_To (
-        closet_id       INTEGER, //still don't know if thats the right syntax...
+        closet_id       INTEGER, --still don't know if thats the right syntax...
         shelf_id        INTEGER,
         container_id    INTEGER,
-        PRIMARY KEY (closet_id, shelf_id, container_id) //still don't know if this is the right syntax either...
+        PRIMARY KEY (closet_id, shelf_id, container_id) --still don't know if this is the right syntax either...
         FOREIGN KEY (closet_id) REFERENCES closet(Cid)
         FOREIGN KEY (shelf_id) REFERENCES shelf(Sid)
-        FOREIGN KEY (container_id) REFERENCES (//fill this in once container has been made)
-        //to be finished
+        --FOREIGN KEY (container_id) REFERENCES (fill this in once container has been made)
+        --to be finished
     );
 
     CREATE TABLE IF NOT EXISTS Contains_Item (
         container_id    
         shelf_id        
         item_id     
-        //to be finished
+        --to be finished
     );
 
 `);
