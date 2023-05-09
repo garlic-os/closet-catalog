cfa3b98bcc839ec91527d33adeb92bed694ddb89
the-garlic-os <14845347+garlic-os@users.noreply.github.com>
2023-05-09 14:01:15 -0500

    Update containerview.js

--- src/screens/ContainerView/containerview.js
+++ src/screens/ContainerView/containerview.js
@@ -85,7 +85,7 @@ class Container extends React.Component {
                         return (
                         <div id="containerviewitem" onClick={() => this.handleItemClick(item)}>
                             <h4>{item["name"]}</h4>
-                            <img src={item.photo_url.replace("public", "")} alt="No Picture" height="50px" />
+                            <img src={item?.photo_url?.replace("public", "")} alt="No Picture" height="50px" />
                             <br></br>
                         </div>
                         );
