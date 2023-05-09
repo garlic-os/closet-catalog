ddfe31616ecaed724a94167e93fa546bf04fd717
the-garlic-os <14845347+garlic-os@users.noreply.github.com>
2023-05-09 03:08:50 -0500

    Add Item: Update container based on selected shelf

--- src/components/addobjectcards/additemcard.js
+++ src/components/addobjectcards/additemcard.js
@@ -24,85 +24,91 @@ async function addItem(event) {
 }
 
 
-function AddItemCard (props) {
-    const items = [
-        {
-            name: "Name",
-            slug: "name",
-            type: "text"
         },
-        {
-            name: "Picture",
-            slug: "photo",
-            type: "file"
-        },
-        {
-            name: "Quantity",
-            slug: "initialCount",
-            type: "number"
-        },
-        {
-            name: "Type",
-            slug: "itemTypeName",
-            type: "text"
-        },
-        {
-            name: "Expiration Date",
-            slug: "expirationDate",
-            type: "date"
-        }
-    ]
+class AddItemCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.state = { selectedShelf: this.props.closetData.shelves[0] };
+        this.items = [
+            {
+                name: "Name",
+                slug: "name",
+                type: "text"
+            },
+            {
+                name: "Picture",
+                slug: "photo",
+                type: "file"
+            },
+            {
+                name: "Quantity",
+                slug: "initialCount",
+                type: "number"
+            },
+            {
+                name: "Type",
+                slug: "itemTypeName",
+                type: "text"
+            },
+            {
+                name: "Expiration Date",
+                slug: "expirationDate",
+                type: "date"
+            }
+        ]
+    };
 
-    // TODO: get shelves and containers from database
-    let shelvesAndContainers = [
-        {
-            name: "Default Shelf",
-            id: 0
-        },
-    ];
-
-    const handleButton = () => {
+    handleButton() {
         eventBus.dispatch("cancel adding item", {message: "cancel adding item"});
     }
 
-    let count = 0;
-    return (
-        <div id="itemcard">
-            <h2>Add an Item</h2>
-            <p>Note: All attributes must be filled upon creation.</p>
-            <form id="itemform" onSubmit={addItem}>
-                {
-                    items.map(({ name, slug, type }) =>
-                        <div key={count++}>
-                            <b>{name}: </b>
-                            <input name={slug} type={type}></input>
-                            <br/><br/>
-                        </div>
-                    )
-                }
-                <b>shelves: </b>
-                <select id="shelves" name="shelf_id">
+    handleSelectShelf(event) {
+        const shelf_id = parseInt(event.target.value);
+        const selectedShelf = this.props.closetData.shelves
+            .find(shelf => shelf.shelf_id === shelf_id);
+        this.setState({ selectedShelf });
+    }
+
+    render() {
+        return (
+            <div id="itemcard">
+                <h2>Add an Item</h2>
+                <p>Note: All attributes must be filled upon creation.</p>
+                <form id="itemform" onSubmit={addItem}>
                     {
-                        props.closetData.shelves.map(({ name, shelf_id }) =>
-                            <option key={count++} value={shelf_id}>{name}</option>
+                        this.items.map(({ name, slug, type }) =>
+                            <div key={this.count++}>
+                                <b>{name}: </b>
+                                <input name={slug} type={type} />
+                                <br/><br/>
+                            </div>
                         )
                     }
-                </select>
-                <br></br>
-                <b>containers: </b>
-                <select id="containers" name="container_id">
-                    {
-                        props.closetData.containers.map(({ name, container_id }) =>
-                            <option key={count++} value={container_id}>{name}</option>
-                        )
-                    }
-                </select>
-                <br/><br/>
-                <button className="button" type="button" onClick={handleButton}>Cancel</button>
-                <input className="button" type="submit" value="Add Item" />
-            </form>
-        </div>
-    )
+                    <b>shelves: </b>
+                    <select id="shelves" name="shelf_id" onChange={this.handleSelectShelf.bind(this)}>
+                        {
+                            this.props.closetData.shelves.map(({ name, shelf_id }) =>
+                                <option key={this.count++} value={shelf_id}>{name}</option>
+                            )
+                        }
+                    </select>
+                    <br></br>
+                    <b>containers: </b>
+                    <select id="containers" name="container_id">
+                        {
+                            this.state.selectedShelf.containers.map(({ name, container_id }) =>
+                                <option key={this.count++} value={container_id}>{name}</option>
+                            )
+                        }
+                    </select>
+                    <br/><br/>
+                    <button className="button" type="button" onClick={this.handleButton}>Cancel</button>
+                    <input className="button" type="submit" value="Add Item" />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default AddItemCard;
